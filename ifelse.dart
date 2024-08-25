aa7a09ad449e64388b30350707d876f0b7026080
wazir1420 <wazirbalti1@gmail.com>
2024-08-25 13:56:51 +0500

    done

---
+++ ifelse.dart
@@ -0,0 +1,23 @@
+void main(){
+  int math = 89;
+  int eng = 90;
+  int chemistry = 88;
+  int phy = 67;
+  int computer = 84;
+  int obtainedMarks = math+eng+chemistry+phy+computer;
+  int totalMarks = 500;
+  num percentage = (obtainedMarks/totalMarks)*100;
+  if(percentage<50){
+    print('F Grade FAIL');
+  }else if(percentage>=50 && percentage<60){
+    print('C Grade');
+  }else if(percentage>=60 && percentage<70){
+    print('B Grade');
+  }else if(percentage>=70 && percentage<80){
+    print('A Grade');
+  }else if(percentage>=80 && percentage<=100){
+    print('A+ Grade');
+  }else{
+    print('Invalid percentage');
+  }
+  }
